e254d8966e25db509a5fd63f9b60f7d57a5f7f37
Lewis <dsapione@gmail.com>
2021-12-19 10:31:02 -0500

    remove tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,49 +112,61 @@ $(".list-group").on("blur", "input[type='text']", function() {
   var date = $(this)
     .val()
     .trim();
-
-  // get the parent ul's id attribute
-  var status = $(this)
+		
+		// get the parent ul's id attribute
+		var status = $(this)
     .closest(".list-group")
     .attr("id")
     .replace("list-", "");
-
-  // get the task's position in the list of other li elements
-  var index = $(this)
+		
+		// get the task's position in the list of other li elements
+		var index = $(this)
     .closest(".list-group-item")
     .index();
-
-  // update task in array and re-save to localstorage
-  tasks[status][index].date = date;
-  saveTasks();
-
-  // recreate span element with bootstrap classes
-  var taskSpan = $("<span>")
+		
+		// update task in array and re-save to localstorage
+		tasks[status][index].date = date;
+		saveTasks();
+		
+		// recreate span element with bootstrap classes
+		var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(date);
-
-  // replace input with span element
-  $(this).replaceWith(taskSpan);
-});
-
-
-
-// modal was triggered
-$("#task-form-modal").on("show.bs.modal", function() {
-  // clear values
-  $("#modalTaskDescription, #modalDueDate").val("");
-});
-
-// modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function() {
-  // highlight textarea
-  $("#modalTaskDescription").trigger("focus");
-});
-
-// save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
+		
+		// replace input with span element
+		$(this).replaceWith(taskSpan);
+	});
+	
+	$("#trash").droppable({
+		accept: ".card .list-group-item",
+		tolerance: "touch",
+		drop: function(event, ui) {
+			ui.draggable.remove();
+		},
+		over: function(event, ui) {
+			console.log("over");
+		},
+		out: function(event, ui) {
+			console.log("out");
+		}
+	});
+	
+	// modal was triggered
+	$("#task-form-modal").on("show.bs.modal", function() {
+		// clear values
+		$("#modalTaskDescription, #modalDueDate").val("");
+	});
+	
+	// modal is fully visible
+	$("#task-form-modal").on("shown.bs.modal", function() {
+		// highlight textarea
+		$("#modalTaskDescription").trigger("focus");
+	});
+	
+	// save button in modal was clicked
+	$("#task-form-modal .btn-primary").click(function() {
+		// get form values
+		var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
 
   if (taskText && taskDate) {
@@ -173,13 +185,62 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+$(".card .list-group").sortable({
+	connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+		console.log("activate", this);
+  },
+  deactivate: function(event) {
+		console.log("deactivate", this);
+  },
+  over: function(event) {
+		console.log("over", event.target);
+  },
+  out: function(event) {
+		console.log("out", event.target);
+  },
+	update: function(event) {
+		// array to store the task data in
+		var tempArr = [];
+		
+		// loop over current set of children in sortable list
+		$(this).children().each(function() {
+			// save values in temp array
+			tempArr.push({
+				text: $(this)
+				.find("p")
+				.text()
+				.trim(),
+				date: $(this)
+				.find("span")
+				.text()
+				.trim()
+			});
+		});
+		
+		// trim down list's ID to match object property
+		var arrName = $(this)
+		.attr("id")
+		.replace("list-", "");
+		
+		// update array on tasks object and save
+		tasks[arrName] = tempArr;
+		saveTasks();
+		
+		console.log(tempArr);
+	}
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
-  for (var key in tasks) {
-    tasks[key].length = 0;
-    $("#list-" + key).empty();
-  }
-  saveTasks();
+	for (var key in tasks) {
+		tasks[key].length = 0;
+		$("#list-" + key).empty();
+	}
+	saveTasks();
 });
 
 // load tasks for the first time
